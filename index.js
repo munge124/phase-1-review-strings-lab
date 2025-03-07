d0e4d8e747d338536c9a8e0a5d63062448782c96
benson <bensonmunge545@gmail.com>
2025-03-07 08:18:14 +0300

    creates index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+console.log(welcomeMessage);  // "Welcome to Flatbook, Grace Hopper!"
+console.log(excitedWelcomeMessage);  // "WELCOME TO FLATBOOK, GRACE HOPPER!"
+console.log(shortGreeting);  // "Welcome, G!"
+
